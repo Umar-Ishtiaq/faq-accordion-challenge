8a34cc1a92bde8aab67090aadc734e87a5d7ac5d
Umar-Ishtiaq <102531467+Umar-Ishtiaq@users.noreply.github.com>
2023-12-12 22:49:56 +0500

    Update main.js
    Added another way for displaying answers

--- main.js
+++ main.js
@@ -13,3 +13,17 @@ plus.forEach(image => {
     }
     })
 })
+
+/* plus.forEach(image => {
+    image.addEventListener('click', ()=> {
+        const id = image.id
+        const e = document.querySelector(`p#${id}`)
+    if(e.classList.contains('active')){
+        image.src = './assets/images/icon-plus.svg'
+        e.classList.remove('active')
+    } else {
+        e.classList.add('active')
+        image.src = './assets/images/icon-minus.svg'
+    }
+    })
+}) */
